94240151652272b9de2637453e212e53e4d12ace
unknown <sugambaskota@gmail.com>
2019-09-03 18:18:21 +0545

    reach logoutAll

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 
@@ -24,13 +25,30 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
+try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+        return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+} catch (e) {
+    res.status(500).send();
+}
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
     try {
-        const users = await User.find({});
-        res.status(200).send(users);
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
     } catch (e) {
-        res.status(500).send(e);
+        res.status(500).send();
     }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
