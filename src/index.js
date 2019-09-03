e2b80f47c59a1b06af2f9655ab4114e2b38c24fe
Sugam Baskota <sugambaskota@gmail.com>
2019-09-03 09:49:04 +0545

    reach express middleware

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled');
+//     } else {
+//         next();
+//     }
+// });
 
-app.use((req, res, next) => {
-    
-});
 
 
 app.use(express.json());
